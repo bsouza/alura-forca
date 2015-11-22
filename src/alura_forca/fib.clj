f18a18018c7451b17f88bc6fcc1a343a89285c24
Bruno Souza <to.bsouza@gmail.com>
2015-11-22 20:00:46 -0200

    melhorando implementacao de fibonacci

--- src/alura_forca/fib.clj
+++ src/alura_forca/fib.clj
@@ -1,14 +1,10 @@
 (ns alura-forca.fib
   (:gen-class))
 
-(def zero-ou-um #{0 1})
-
-(defn eh-zero-ou-um [numero]
-  (contains? zero-ou-um numero)
-)
+(defn maior-que-um? [numero] (> numero 1))
 
 (defn fib [numero]
-  (if (not (zero-ou-um numero))
+  (if (maior-que-um? numero)
     (+ (fib (dec numero)) (fib (- numero 2)))
     numero
   )
