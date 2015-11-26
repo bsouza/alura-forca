eb42db5d42747743aa321319d8e091fa040f7962
Bruno Souza <to.bsouza@gmail.com>
2015-11-25 23:11:19 -0200

    final da aula 5 com tail recursion

--- src/alura_forca/core.clj
+++ src/alura_forca/core.clj
@@ -1,34 +1,38 @@
-(ns alura-forca.core
-  (:gen-class))
+  (ns alura-forca.core
+    (:gen-class))
 
-(def total-de-vidas 6)
+  (def total-de-vidas 6)
 
-(defn perdeu [] (println "Você perdeu!"))
-(defn ganhou [] (println "Você ganhou!"))
+  (defn perdeu [] (println "Você perdeu!"))
+  (defn ganhou [] (println "Você ganhou!"))
 
-(defn letras-faltantes [palavra acertos]
-  (remove (fn [letra] (contains? acertos (str letra))) palavra))
+  (defn letras-faltantes [palavra acertos]
+    (remove (fn [letra] (contains? acertos (str letra))) palavra))
 
-(defn acertou-palavra-toda? [palavra acertos]
-  (empty? (letras-faltantes palavra acertos)))
+  (defn acertou-palavra-toda? [palavra acertos]
+    (empty? (letras-faltantes palavra acertos)))
 
-(defn le-letra! [] (read-line))
+  (defn le-letra! []
+    (read-line))
 
-(defn acertou? [chute palavra] (.contains palavra chute))
+  (defn acertou? [chute palavra]
+    (.contains palavra chute))
 
-(defn avalia-chute [chute vidas palavra acertos]
-  (if (acertou? chute palavra)
-    (jogo vidas palavra (conj acertos chute))
-    (jogo (dec vidas) palavra acertos)))
+  (defn jogo [vidas palavra acertos]
+    (cond
+      (= vidas 0) (perdeu)
+      (acertou-palavra-toda? palavra acertos) (ganhou)
+      :else
+      (let [chute (le-letra!)]
+        (if (acertou? chute palavra)
+          (do
+            (println "Acertou letra!")
+            (recur vidas palavra (conj acertos chute)))
+          (do
+            (println "Errou! Perdeu vida!")
+            (recur (dec vidas) palavra acertos))))))
 
-(defn jogo [vidas palavra acertos]
-  (if (= vidas 0)
-    (perdeu)
-    (if (acertou-palavra-toda? palavra acertos)
-      (ganhou)
-      (avalia-chute (le-letra!) vidas palavra acertos))))
-
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+  (defn -main
+    "I don't do a whole lot ... yet."
+    [& args]
+    (println "Hello, World!"))
--- src/alura_forca/fib.clj
+++ src/alura_forca/fib.clj
@@ -3,9 +3,24 @@
 
 (defn maior-que-um? [numero] (> numero 1))
 
-(defn fib [numero]
+(defn fib-old [numero]
   (if (maior-que-um? numero)
-    (+ (fib (dec numero)) (fib (- numero 2)))
+    (+ (fib-old (dec numero)) (fib-old (- numero 2)))
     numero
   )
 )
+
+
+(defn fib[x]
+    (loop [a 1 b 1 numero 2]
+        (if
+            (= numero x) b
+            (recur b (+ a b) (inc numero))
+        )
+    ))
+
+
+(defn soma[n]
+  (loop [contador 1 soma 0]
+    (if (> contador n) soma
+    (recur (inc contador) (+ contador soma)))))
