2280c06613e3f3566aa49ce016136cf6e96abfe5
Bruno Souza <to.bsouza@gmail.com>
2015-12-01 14:49:59 -0200

    final aula 6 -> funções preguiçosas :sleeping:

--- src/alura_forca/core.clj
+++ src/alura_forca/core.clj
@@ -18,7 +18,15 @@
   (defn acertou? [chute palavra]
     (.contains palavra chute))
 
+  (defn imprime-forca [vidas palavra acertos]
+    (println "Vidas " vidas)
+    (doseq [letra (seq palavra)]
+      (if (contains? acertos (str letra))
+        (print letra " ") (print "_" " ")))
+    (println))
+
   (defn jogo [vidas palavra acertos]
+    (imprime-forca vidas palavra acertos)
     (cond
       (= vidas 0) (perdeu)
       (acertou-palavra-toda? palavra acertos) (ganhou)
